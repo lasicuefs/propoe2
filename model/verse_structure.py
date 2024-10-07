4d38599c23845b158a43246766cbab020dd4224a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-10-07 17:25:45 -0300

    refactor(model): add annotations

--- model/verse_structure.py
+++ model/verse_structure.py
@@ -5,21 +5,24 @@ from .utils import (
 )
 from .utils import left_consonant_removal
 
+type AccentationKind = (
+    Literal["Esdrúxula"] | Literal["Grave"] | Literal["Aguda"]
+)
 
 class VerseStructure:
     def __init__(
         self, sentence, syllable_number, stress_position, scanned_sentence
-    ):
-        self.metric = int(syllable_number)
+    ) -> None:
+        self.metric: int = int(syllable_number)
         self.stress_position = stress_position.split()
         self.scanned_sentence = scanned_sentence
-        self.accent = self.accentuation()
+        self.accent: AccentationKind | None = self.accentuation()
         self.syllables = self.get_syllables()
         self.sentence = sentence
 
         self.stress_syllables, self.pos_stress_dict = self.stress_syllable()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (
             "\n Syllable number: "
             + str(self.metric)
@@ -33,11 +36,11 @@ class VerseStructure:
             + str(self.accent)
         )
 
-    def get_syllables(self):
+    def get_syllables(self) -> list[str]:
         sentence = scanned_sentence_preprocess(self.scanned_sentence)
         return [s.strip() for s in sentence.split("/")]
 
-    def get_last_word(self):
+    def get_last_word(self) -> str:
         sentence = sentence_preprocess(self.sentence)
         word = sentence.split(" ")[-1].strip()
         return word
@@ -45,7 +48,7 @@ class VerseStructure:
     def get_last_stress(self):
         return self.stress_syllables[-1].strip()
 
-    def get_last_syllables(self):
+    def get_last_syllables(self) -> str:
         after = self.syllables[int(self.stress_position[-1]) :]
         stress = self.scanned_sentence.split("/")[
             int(self.stress_position[-1]) - 1
@@ -69,7 +72,7 @@ class VerseStructure:
             pos_syllables[pos] = s.strip()
         return syllables, pos_syllables
 
-    def accentuation(self):
+    def accentuation(self) -> AccentationKind | None:
         """Return accentutation of the sentence."""
         s = remove_end_ponctuation(self.scanned_sentence)
         last_word = s.split()[-1]
