5d40334e5b4124626f6a9196a1d9918cc3a81ae7
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-20 09:49:23 -0300

    test Prosody

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -1,17 +1,17 @@
 from typing import Any, Callable
 import pytest
 
-from src.api import Weights
+from src.api import Prosody, Weights
 
 
 def test(desc: str) -> Callable[..., Any]:
     def wrapper(func: Callable[..., Any]):
         return func
-    return wrapper   
+
+    return wrapper
 
 
 class DescribeWeights:
-
     @staticmethod
     def it_should_init_everything_as_one():
         actual = Weights()
@@ -29,7 +29,7 @@ class DescribeWeights:
             accentuation=2,
             tonic_position=3,
             internal_rhyme=4,
-            rhythmic_structure=5
+            rhythmic_structure=5,
         ).as_dict
 
         assert 1 == actual["Rima toante & consoante"]
@@ -38,3 +38,40 @@ class DescribeWeights:
         assert 4 == actual["Rima interna"]
         assert 5 == actual["Estrutura ritmica"]
 
+
+class DescribeProsody:
+    @staticmethod
+    def it_should_raise_for_larger_metrics():
+        with pytest.raises(AssertionError):
+            Prosody("AB BA", [10] * 5)
+
+    @staticmethod
+    def and_should_also_raise_for_smaller_metrics():
+        with pytest.raises(AssertionError):
+            Prosody("AB BA", [10] * 3)
+
+    @staticmethod
+    def but_should_not_raise_for_same_size():
+        Prosody("AB BA", [10] * 4)
+
+    @staticmethod
+    @test(
+        "Should ignore whitespaces when analizying the lenght of rhythm and rhymes"
+    )
+    def and_should_ignore_whitespace_for_rhymes():
+        """
+        This does not assert anything, because this is testing the AssertionError raising.
+        Notice that both "AB BA" and "ABBA" are considered as being 4-lenght long rhymes.
+        """
+        assert Prosody("AB BA", [10] * 4) is not None
+        assert Prosody("ABBA", [10] * 4) is not None
+
+    @staticmethod
+    def its_attributes_are_available():
+        rhythm = [10] * 4
+        rhyme = "AB BA"
+        actual = Prosody("AB BA", [10] * 4)
+
+        assert rhythm == actual.rhythm
+        assert rhyme == actual.rhyme
+        assert actual.rhyme == actual.pattern
