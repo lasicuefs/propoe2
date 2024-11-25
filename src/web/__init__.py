01edd61257d567223210b2acc6eaedf358a9c155
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-25 20:09:40 -0300

    add initial API modeling

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,8 +1,42 @@
 from fastapi import FastAPI
 
+from src.api import Poem, Propoe, Prosody, Weights
+
 app = FastAPI()
 
+@app.get("/poem/")
+async def poem(prosody: Prosody, weights: Weights = Weights()):
+    poem = Propoe(
+        filename="poem_test_api.txt",
+        mives_file="xml/sentencas.xml",
+        prosody=prosody,
+        evaluation_weights=weights
+    ).poem
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    return {
+        "status": "OK",
+        "content": poem.content,
+        "evaluation": poem.evaluation,
+    }
+
+
+@app.get("/sample/")
+async def sample():
+    prosody = Prosody(
+        "ABAB ABAB CDC CDC",
+        [10] * 14,
+    )
+
+    weights = Weights()
+
+    poem = Propoe(
+        filename="poem_test_api.txt",
+        mives_file="xml/sentencas.xml",
+        prosody=prosody,
+        evaluation_weights=weights,
+    ).poem
+
+    return {
+        "content": poem.content,
+        "evaluation": poem.evaluation,
+    }
