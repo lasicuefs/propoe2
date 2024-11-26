a889f6f9dcbd3bdeb49653ead90457ce8517f792
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-26 20:14:45 -0300

    implement Weights Schema

--- src/web/schemas/__init__.py
+++ src/web/schemas/__init__.py
@@ -1,1 +1,2 @@
 from src.web.schemas.poem import *  # noqa: F403
+from src.web.schemas.weights import *  # noqa: F403
---
+++ src/web/schemas/weights.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field
+
+from src import api as domain
+
+__all__ = ["Weights"]
+
+
+class Weights(BaseModel):
+    vocal_harmony: float = Field(default=1, ge=0, le=1)
+    accentuation: float = Field(default=1, ge=0, le=1)
+    tonic_position: float = Field(default=1, ge=0, le=1)
+    internal_rhyme: float = Field(default=1, ge=0, le=1)
+    rhythmic_structure: float = Field(default=1, ge=0, le=1)
+
+    def as_domain(self) -> domain.Weights:
+        return domain.Weights(
+            accentuation=self.accentuation,
+            internal_rhyme=self.internal_rhyme,
+            rhythmic_structure=self.rhythmic_structure,
+            tonic_position=self.tonic_position,
+            vocal_harmony=self.vocal_harmony,
+        )
+
+    class Config:
+        @staticmethod
+        def to_kebab(x: str) -> str:
+            return x.replace("_", "-")
+
+        alias_generator = to_kebab
+        allow_population_by_field_name = True
