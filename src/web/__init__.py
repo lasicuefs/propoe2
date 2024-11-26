bac5463687168fd26dbaf8d476d80dbfb862cd36
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-26 19:36:14 -0300

    use Poem Schema on API

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,27 +1,25 @@
 from fastapi import FastAPI
 
-from src.api import Poem, Propoe, Prosody, Weights
+from src.api import Propoe, Prosody, Weights
+from src.web import schemas
 
 app = FastAPI()
 
+
 @app.get("/poem/")
-async def poem(prosody: Prosody, weights: Weights = Weights()):
+async def poem(prosody: Prosody, weights: Weights = Weights()) -> schemas.Poem:
     poem = Propoe(
         filename="poem_test_api.txt",
         mives_file="xml/sentencas.xml",
         prosody=prosody,
-        evaluation_weights=weights
+        evaluation_weights=weights,
     ).poem
 
-    return {
-        "status": "OK",
-        "content": poem.content,
-        "evaluation": poem.evaluation,
-    }
+    return schemas.Poem.from_poem(poem)
 
 
 @app.get("/sample/")
-async def sample():
+async def sample() -> schemas.Poem:
     prosody = Prosody(
         "ABAB ABAB CDC CDC",
         [10] * 14,
@@ -36,7 +34,4 @@ async def sample():
         evaluation_weights=weights,
     ).poem
 
-    return {
-        "content": poem.content,
-        "evaluation": poem.evaluation,
-    }
+    return schemas.Poem.from_poem(poem)
