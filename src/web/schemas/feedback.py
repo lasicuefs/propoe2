dfeb9dccc206f1c2f899727014bc957411daa969
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-03 19:05:42 -0300

    [web] simplify Feedback's Schema

--- src/web/schemas/feedback.py
+++ src/web/schemas/feedback.py
@@ -1,36 +1,26 @@
 from pydantic import BaseModel, field_validator
 
 
-class Stars(BaseModel):
-    """User's evaluation of Propoe that goes into ``Feedback``."""
-    value: int
+class Feedback(BaseModel):
+    """User's feedback about Propoe."""
+    stars: int
+    comment: str
 
-    @field_validator("value")
+    @field_validator("stars")
     @classmethod
     def value_must_be_between_1_and_5(cls, value: int) -> int:
-        """``value`` must be between 1 and 5."""
+        """Must be between 1 and 5."""
         if not 1 <= value <= 5:
             raise ValueError("Stars evaluation must be between 1 and 5.")
         return value
-
-
-class Comment(BaseModel):
-    """User's comment that goes into a ``Feedback``"""
-    content: str
-
-    @field_validator("content")
+    
+    @field_validator("comment")
     @classmethod
     def content_must_be_big_as_a_tweet(cls, value: str) -> str:
-        """``content`` must not be bigger than a Tweet."""
+        """Must not be bigger than a Tweet."""
         CHARACTER_LIMIT = 280
         if len(value) > CHARACTER_LIMIT:
             raise ValueError(
                 f"content must be up to {CHARACTER_LIMIT} characters."
             )
         return value
-
-
-class Feedback(BaseModel):
-    """User's feedback about Propoe."""
-    stars: Stars
-    comment: Comment
