d3702dc758baab3035b25e80e35244ee99d42727
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-03 19:24:45 -0300

    [web] simplify Feedback's Schema use Fields instead of manual validation

--- src/web/schemas/feedback.py
+++ src/web/schemas/feedback.py
@@ -1,26 +1,15 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, Field, field_validator
 
 
 class Feedback(BaseModel):
-    """User's feedback about Propoe."""
-    stars: int
-    comment: str
-
-    @field_validator("stars")
-    @classmethod
-    def value_must_be_between_1_and_5(cls, value: int) -> int:
-        """Must be between 1 and 5."""
-        if not 1 <= value <= 5:
-            raise ValueError("Stars evaluation must be between 1 and 5.")
-        return value
+    """User's feedback about Propoe.
     
-    @field_validator("comment")
-    @classmethod
-    def content_must_be_big_as_a_tweet(cls, value: str) -> str:
-        """Must not be bigger than a Tweet."""
-        CHARACTER_LIMIT = 280
-        if len(value) > CHARACTER_LIMIT:
-            raise ValueError(
-                f"content must be up to {CHARACTER_LIMIT} characters."
-            )
-        return value
+    Attributes
+    ----------
+    stars: int 
+        Stars evaluation, should be between 1 and 5.
+    comment: str 
+        Comment content, should have the size of a Tweet, 280 characters.
+    """
+    stars: int = Field(strict=True, ge=1, le=5, description="Stars evaluation")
+    comment: str = Field(strict=True, max_length=280, description="Feedback content")
