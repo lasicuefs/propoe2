a05def1a7881d6f73a49cd2479113f25a783964f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-03 20:54:56 -0300

    [web\schema] add example to Feedback

--- src/web/schemas/feedback.py
+++ src/web/schemas/feedback.py
@@ -5,13 +5,21 @@ from pydantic import BaseModel, Field
 
 class Feedback(BaseModel):
     """User's feedback about Propoe.
-    
+
     Attributes
     ----------
-    stars: int 
+    stars: int
         Stars evaluation, should be between 1 and 5.
-    comment: str 
+    comment: str
         Comment content, should have the size of a Tweet, 280 characters.
     """
-    stars: int = Field(strict=True, ge=1, le=5, description="Stars evaluation")
-    comment: str = Field(strict=True, max_length=280, description="Feedback content")
+
+    stars: int = Field(
+        examples=["5"], strict=True, ge=1, le=5, description="Stars evaluation"
+    )
+    comment: str = Field(
+        examples=["Excelent, Propoe2 is awesome!"],
+        strict=True,
+        max_length=280,
+        description="Feedback content",
+    )
