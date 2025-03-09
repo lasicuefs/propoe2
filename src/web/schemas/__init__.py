c5299dba35d45e62d55713f7289dd6d9ca192cc6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-09 14:20:10 -0300

    [web\schema] update imports

--- src/web/schemas/__init__.py
+++ src/web/schemas/__init__.py
@@ -1,3 +1,2 @@
 from src.web.schemas.poem import *  # noqa: F403
-from src.web.schemas.weights import *  # noqa: F403
-from src.web.schemas.prosody import *  # noqa: F403
+from src.web.schemas.poem_forms import *  # noqa: F403
