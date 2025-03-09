33281fe8a6148c72a609416b3ea3bbb6faaee6c4
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-09 14:48:39 -0300

    [web\schema] add docs and add noqa

--- src/web/schemas/__init__.py
+++ src/web/schemas/__init__.py
@@ -1,3 +1,5 @@
-from src.web.schemas.poem_result import Poem
-from src.web.schemas.poem_forms import PoemForms
-from src.web.schemas.feedback import Feedback
+"""Schemas used by Propoe2's API"""
+
+from src.web.schemas.poem_result import Poem  # noqa: F401
+from src.web.schemas.poem_forms import PoemForms  # noqa: F401
+from src.web.schemas.feedback import Feedback  # noqa: F401
